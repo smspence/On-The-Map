f45aaba93d9268ce36e6b0b8f38fd3177c9d551c
Shawn Spencer <shawns333@gmail.com>
2015-07-15 20:17:15 -0500

    Got map view and table view working with hard coded location data

--- OnTheMap/ParseClient.swift
+++ OnTheMap/ParseClient.swift
@@ -10,4 +10,87 @@ import Foundation
 
 class ParseClient : NSObject {
 
+    /* Shared session */
+    var session: NSURLSession
+
+    override init() {
+        session = NSURLSession.sharedSession()
+        super.init()
+    }
+
+
+// Example for how to get student locations from Parse:
+//    let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation")!)
+//    request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+//    request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+//    let session = NSURLSession.sharedSession()
+//    let task = session.dataTaskWithRequest(request) { data, response, error in
+//        if error != nil { // Handle error...
+//            return
+//        }
+//        println(NSString(data: data, encoding: NSUTF8StringEncoding))
+//    }
+//    task.resume()
+
+
+    func hardCodedLocationData() -> [[String : AnyObject]] {
+        return  [
+            [
+                "createdAt" : "2015-02-24T22:27:14.456Z",
+                "firstName" : "Jessica",
+                "lastName" : "Uelmen",
+                "latitude" : 28.1461248,
+                "longitude" : -82.75676799999999,
+                "mapString" : "Tarpon Springs, FL",
+                "mediaURL" : "www.linkedin.com/in/jessicauelmen/en",
+                "objectId" : "kj18GEaWD8",
+                "uniqueKey" : 872458750,
+                "updatedAt" : "2015-03-09T22:07:09.593Z"
+            ], [
+                "createdAt" : "2015-02-24T22:35:30.639Z",
+                "firstName" : "Gabrielle",
+                "lastName" : "Miller-Messner",
+                "latitude" : 35.1740471,
+                "longitude" : -79.3922539,
+                "mapString" : "Southern Pines, NC",
+                "mediaURL" : "http://www.linkedin.com/pub/gabrielle-miller-messner/11/557/60/en",
+                "objectId" : "8ZEuHF5uX8",
+                "uniqueKey" : 2256298598,
+                "updatedAt" : "2015-03-11T03:23:49.582Z"
+            ], [
+                "createdAt" : "2015-02-24T22:30:54.442Z",
+                "firstName" : "Jason",
+                "lastName" : "Schatz",
+                "latitude" : 37.7617,
+                "longitude" : -122.4216,
+                "mapString" : "18th and Valencia, San Francisco, CA",
+                "mediaURL" : "http://en.wikipedia.org/wiki/Swift_%28programming_language%29",
+                "objectId" : "hiz0vOTmrL",
+                "uniqueKey" : 2362758535,
+                "updatedAt" : "2015-03-10T17:20:31.828Z"
+            ], [
+                "createdAt" : "2015-03-11T02:48:18.321Z",
+                "firstName" : "Jarrod",
+                "lastName" : "Parkes",
+                "latitude" : 34.73037,
+                "longitude" : -86.58611000000001,
+                "mapString" : "Huntsville, Alabama",
+                "mediaURL" : "https://linkedin.com/in/jarrodparkes",
+                "objectId" : "CDHfAy8sdp",
+                "uniqueKey" : 996618664,
+                "updatedAt" : "2015-03-13T03:37:58.389Z"
+            ]
+        ]
+    }
+
+    // MARK: - Shared Instance
+
+    class func sharedInstance() -> ParseClient {
+
+        struct Singleton {
+            static var sharedInstance = ParseClient()
+        }
+
+        return Singleton.sharedInstance
+    }
 }
--- OnTheMap/TableViewController.swift
+++ OnTheMap/TableViewController.swift
@@ -8,9 +8,45 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    // TODO - use this cell reuse id:
-    // "OTMTableViewReuseId"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ParseClient.sharedInstance().hardCodedLocationData().count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCellWithIdentifier("OTMTableViewReuseId") as! UITableViewCell
+        let location = ParseClient.sharedInstance().hardCodedLocationData()[indexPath.row]
+
+        // Set the name and image
+        if let firstName = location["firstName"] as? String,
+            let lastName  = location["lastName"] as? String {
+
+                cell.textLabel?.text = firstName + " " + lastName
+        } else {
+            // TODO - remove, find better way to show error
+            cell.textLabel?.text = "Error"
+        }
+
+        return cell
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Show the detail view of the selected location
+
+//        detailViewIndexPath = indexPath
+//
+//        let detailVC = self.storyboard!.instantiateViewControllerWithIdentifier("MemeDetailViewStoryboardId") as! MemeDetailViewController
+//        detailVC.meme = self.memes[indexPath.item]
+//        detailVC.deletionDelegate = self
+//        detailVC.hidesBottomBarWhenPushed = true
+//        self.navigationController!.pushViewController(detailVC, animated: true)
+    }
 
 }
